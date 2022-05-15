69c69cbc8a02dbfbfe35d347dc105b6260499bb3
kmelter <kmelter97@gmail.com>
2022-05-15 13:27:49 -0500

    update post

---
+++ models/index.js
@@ -0,0 +1,69 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const UserPost = require('./UserPost')
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+User.belongsToMany(Post, {
+  through: UserPost,
+  as: 'user_posts',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+  through: UserPost,
+  as: 'user_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+UserPost.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+UserPost.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(UserPost, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(UserPost, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, UserPost, Comment };
