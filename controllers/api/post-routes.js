d0f453aa6d7b003f8f19bec8ad51886ea264577f
kmelter <kmelter97@gmail.com>
2022-05-15 15:04:04 -0500

    update post contents

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,7 +90,8 @@ router.post('/', withAuth, (req, res) => {
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
-      title: req.body.title
+      title: req.body.title,
+      post_contents: req.body.post_contents
     },
     {
       where: {
