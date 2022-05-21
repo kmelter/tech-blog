e568312c4bd7feb43950b58e0f207732d75f5d47
kmelter <kmelter97@gmail.com>
2022-05-21 10:08:56 -0500

    define Sequelize

--- config/connection.js
+++ config/connection.js
@@ -18,7 +18,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
+const Sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
@@ -26,4 +26,4 @@ const sequelize = process.env.JAWSDB_URL
       port: 3306
 });
 
-module.exports = sequelize;
+module.exports = Sequelize;
